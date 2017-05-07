a0d513879428a106f51acc6c6db8f0caf6c02a8a
Hermann Arriagada Mendez <hylianred@arrias.cl>
2017-05-07 20:17:10 -0300

    Mensaje Jutana Project 2017 en el index.php

--- web/index.php
+++ web/index.php
@@ -1,8 +1,11 @@
-<?php
-phpinfo();
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Jutana Project 2017</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <div>Jutana Project 2017</div>
+    </body>
+</html>
